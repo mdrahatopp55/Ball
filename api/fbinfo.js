b23ce2ee2857b2fe9bcb1c999475ec20f48068c9
mdrahatopp55 <mdrahatopp@gmail.com>
2025-11-27 13:37:04 +0600

    Update fbinfo.js

--- api/fbinfo.js
+++ api/fbinfo.js
@@ -1,41 +1,43 @@
-import { ApifyClient } from 'apify-client';
+import { ApifyClient } from "apify-client";
 
 export default async function handler(req, res) {
     res.setHeader("Content-Type", "application/json; charset=utf-8");
 
+    // Allow only GET
     if (req.method !== "GET") {
         return res.status(405).json({
             success: false,
             message: "Only GET method allowed",
         });
-    }
+    } // <-- FIXED missing bracket
 
     const pageUrl = req.query.url;
 
     if (!pageUrl) {
         return res.status(400).json({
             success: false,
-            message: "Please provide URL. Example: /api/run?url=https://facebook.com/page"
+            message: "Please provide URL. Example: /api/run?url=https://facebook.com/page",
         });
     }
 
     try {
         const client = new ApifyClient({
-            token: "apify_api_lXQd5BhJyPBel2TLkPkfj2vCXgAgk13tfVt9", // Your Token Added ✔
+            token: "apify_api_lXQd5BhJyPBel2TLkPkfj2vCXgAgk13tfVt9", // Token Added ✔
         });
 
         const input = {
-            startUrls: [
-                { url: pageUrl }
-            ]
+            startUrls: [{ url: pageUrl }],
         };
 
-        // Run the actor (your actor ID)
+        // Run the actor
         const run = await client.actor("4Hv5RhChiaDk6iwad").call(input);
 
         // Fetch results
-        const { items } = await client.dataset(run.defaultDatasetId).listItems();
+        const { items } = await client
+            .dataset(run.defaultDatasetId)
+            .listItems();
 
+        // Response OK
         return res.status(200).json({
             success: true,
             input_url: pageUrl,
@@ -45,6 +47,7 @@ export default async function handler(req, res) {
 
     } catch (err) {
         console.error("Apify error:", err);
+
         return res.status(500).json({
             success: false,
             message: "Something went wrong while running Apify actor",
